5f661e3e280a3c7ab497987e13f43d7d01c4aed6
Carlos Ferreira <c@rlos.rocks>
2017-09-10 17:27:31 +0100

    Gulp babel, uglify and concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,22 @@
 var gulp = require('gulp');
+var babel = require('gulp-babel');
+var sourcemaps = require('gulp-sourcemaps');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+
+// Mind the order of these files
+var files = [
+    'src/MiniYou.js',
+    'src/Creator.js',
+    'src/Editor.js'
+];
 
 gulp.task('default', function () {
-    
+    return gulp.src(files)
+        .pipe(sourcemaps.init())
+        .pipe(babel())
+        .pipe(uglify())
+        .pipe(concat('avatar-creator.js'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('dist'));
 });
